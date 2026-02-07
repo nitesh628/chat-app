5521da9bf1f3c762d4b49b346bb9cfe651e43429
nitesh628 <niteshkumarbokaro628@gmail.com>
2026-02-07 13:08:59 +0530

    final

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,12 +1,12 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import cloudinary from "../lib/cloudinary.js";
-import { getReceiverSocketId } from "../lib/socket.js";
+import { getReceiverSocketId , io} from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
-        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("password");
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } });
 
         res.status(200).json(filteredUsers);
     } catch (error) {
@@ -55,7 +55,7 @@ export const sendMessage = async (req, res) =>{
             image : imageUrl,
         });
 
-        await newMessage.save();
+         newMessage.save();
 
         
         const receiverSocketId = getReceiverSocketId(receiverId);
